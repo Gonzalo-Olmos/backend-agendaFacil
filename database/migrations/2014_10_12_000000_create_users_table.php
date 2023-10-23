c4b26d962d7a8e525be52df57f57bb387972d8ce
Gonzalo Olmos <gonzaloolmos1998@gmail.com>
2023-10-23 00:46:26 -0300

    cambios en migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,26 +8,29 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('proveedores', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('cuit')->unique();
-            $table->string('nombre');
+            $table->string('name');
             $table->string('email')->unique();
-            $table->string('telefono')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('proveedores');
+        Schema::dropIfExists('users');
     }
 };
